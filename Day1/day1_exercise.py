45653fdcb55ede8e2996f90b2f971618090207dd
mangucletus <66282265+mangucletus@users.noreply.github.com>
2022-10-13 22:39:05 +0000

    changes made

---
+++ Day1/day1_exercise.py
@@ -0,0 +1,7 @@
+
+# python code to print string
+print("Day 1 - Python Print Function ")
+print("The function is declared li this: ")
+print("print('what to print') ")
+
+#print("Day 1 - Python Print Function\nThe function is declared li this:\nprint('what to print') ")
---
+++ Day1/day1_exercise2.py
@@ -0,0 +1,11 @@
+# string concatenating(debugging)
+print("Day 1 - String Manipulation")
+print("String concatenation is done with the "+" sign.")
+print('e.g. print("Hello" + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+
+#print("Hello " + input("Please enter your name  "))
+name = input("Enter your full name: ")
+print("How are you today, "+ name)
